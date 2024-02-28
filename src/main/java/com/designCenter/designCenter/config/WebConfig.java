9b5565a62d9e93bd864c92005bcca52e93634c78
ashan_manuka <manukajayarathne.coma@gmail.com>
2024-02-28 14:50:53 +0530

    added configuration

---
+++ src/main/java/com/designCenter/designCenter/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.designCenter.designCenter.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")  // Allow all origins (not recommended for production, tighten the security)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
+                .allowCredentials(true);
+    }
+}
